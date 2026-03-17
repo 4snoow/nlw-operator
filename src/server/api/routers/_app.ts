3013cba09c2d55aa1833797a2d18606ce7f1fdbf
4snoow <gabribs1@hotmail.com>
2026-03-16 23:54:05 -0300

    feat: register roastRouter in appRouter

--- src/server/api/routers/_app.ts
+++ src/server/api/routers/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { leaderboardRouter } from "./leaderboard";
 import { metricsRouter } from "./metrics";
+import { roastRouter } from "./roast";
 
 export const appRouter = router({
 	leaderboard: leaderboardRouter,
 	metrics: metricsRouter,
+	roast: roastRouter,
 });
 
 export type AppRouter = typeof appRouter;
